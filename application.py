bc1e0b18ec7927c8c8f8e02db4ca01b2b4dd0b60
Mert Kaya <2001mk2001@gmail.com>
2020-07-13 17:37:10 -0400

    initial commit

---
+++ application.py
@@ -0,0 +1,75 @@
+import os
+
+from flask import Flask, session, render_template, request, jsonify, redirect, \
+    url_for
+from flask_session import Session
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+chat = {"general": [], "hello": []}
+
+
+@app.route("/")
+def index():
+    if session.get("channelName") is None:
+        session["channelName"] = "general"
+
+    if session.get("username") != "" and session.get("username") is not None:
+        return render_template("index.html")
+
+    return render_template("signin.html")
+
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    session["username"] = request.form.get("username")
+    return redirect(url_for("index"))
+
+
+@socketio.on("send")
+def send(data):
+    chat[data["channelName"]].append(
+        {"sender": data["sender"], "message": data["message"],
+         "sentAt": str(datetime.now().strftime("%H:%M:%S"))})
+    if len(chat[data["channelName"]]) > 100:
+        chat[data["channelName"]].pop(0)
+
+    dataSent = {"channelName": data["channelName"], "sender": data["sender"],
+                "message": data["message"],
+                "sentAt": str(datetime.now().strftime("%H:%M"))}
+
+    emit("update-channel", dataSent, broadcast=True)
+
+
+@app.route("/open", methods=["POST"])
+def open():
+    data = {"username": session["username"],
+            "chat": chat[session["channelName"]],
+            "channelName": session["channelName"],
+            "channels": list(chat.keys())}
+    return jsonify(data)
+
+
+@socketio.on("create-channel")
+def channel_creation(data):
+    if data["channelName"] not in chat.keys() and data["channelName"] != "":
+        chat[data["channelName"]] = []
+        emit("channel-creation", data, broadcast=True)
+
+
+@app.route("/switch-channel", methods=["POST"])
+def switch_channel():
+    session["channelName"] = request.form.get("channelName")
+    data = {"username": session["username"],
+            "chat": chat[session["channelName"]],
+            "channelName": session["channelName"],
+            "channels": list(chat.keys())}
+    return jsonify(data)
---
+++ static/index.js
@@ -0,0 +1,152 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Connect to websocket
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    socket.on("connect", () => {
+        const req = new XMLHttpRequest();
+        req.open("POST", "/open")
+        req.onload = () => {
+            loadPage(JSON.parse(req.responseText));
+        }
+        req.send();
+    })
+
+    socket.on("update-channel", data => {
+        if (localStorage.getItem("channelName") === data.channelName) {
+            document.querySelector('#chat').appendChild(createMessage(data));
+            const listItems = document.querySelector('#chat').querySelectorAll("div");
+            if (listItems.length > 100) {
+                listItems[0].parentElement.removeChild(listItems[0]);
+            }
+            document.querySelector('#chat').scrollTop = document.querySelector('#chat').scrollHeight;
+        }
+    })
+
+    socket.on("channel-creation", data => {
+        createChannelButton(data.channelName)
+    })
+
+    const sendBox = document.querySelector('#form');
+    const textarea = document.querySelector('#textarea');
+
+    sendBox.onsubmit = () => {
+        if (textarea.value.length !== 0) {
+            socket.emit("send", {
+                "channelName": localStorage.getItem("channelName"),
+                "sender": localStorage.getItem("username"),
+                "message": sendBox.querySelector('textarea').value
+            })
+            document.querySelector('#chat').scrollTop = document.querySelector('#chat').scrollHeight;
+        }
+        sendBox.querySelector('textarea').value = "";
+        return false;
+    };
+
+
+    textarea.onkeydown = e => {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            e.preventDefault();
+            document.querySelector('#form').onsubmit();
+        }
+    }
+
+    const newChannel = document.querySelector('#new-channel-name');
+    newChannel.onsubmit = () => {
+      socket.emit("create-channel", {
+        "channelName": newChannel.querySelector('input').value
+      })
+      newChannel.querySelector('input').value = "";
+      return false;
+    };
+
+})
+
+function loadPage(data) {
+    localStorage.setItem("username", data.username)
+
+    loadMessages(data.chat)
+
+    localStorage.setItem("channelName", data.channelName);
+    document.querySelector('#channel-name').innerHTML = data.channelName;
+
+    localStorage.setItem("channels", data.channels);
+    loadChannels(data.channels);
+}
+
+function loadMessages(messages) {
+    const chat = document.querySelector('#chat');
+    chat.innerHTML = "";
+    messages.forEach((item, i) => {
+        chat.appendChild(createMessage(item));
+    });
+    document.querySelector('#chat').scrollTop = document.querySelector('#chat').scrollHeight;
+}
+
+function loadChannels(channelList) {
+    document.querySelector('#buttons').innerHTML = "";
+    channelList.forEach((channelName, i) => {
+        createChannelButton(channelName)
+    });
+}
+
+function createChannelButton(channelName) {
+    const channel = document.createElement("button");
+    channel.className = "list-group-item text-light";
+
+    if (channelName === localStorage.getItem("channelName")) {
+        channel.className += " bg-secondary";
+    } else {
+        channel.className += " bg-dark";
+    }
+
+    channel.onclick = () => {
+        const switchRequest = new XMLHttpRequest();
+        switchRequest.open("POST", "/switch-channel")
+
+        switchRequest.onload = () => {
+            loadPage(JSON.parse(switchRequest.responseText))
+        }
+
+        const sentData = new FormData();
+        sentData.append("channelName", channelName)
+        switchRequest.send(sentData)
+    }
+
+    channel.innerHTML = channelName;
+    document.querySelector('#buttons').appendChild(channel);
+}
+
+function createMessage(messageData) {
+    const container = document.createElement("div");
+    const username = localStorage.getItem("username")
+    const isUserMessage = messageData.sender === username;
+    if (isUserMessage) {
+        container.className = "user-message";
+        container.appendChild(document.createElement("div"));
+    } else {
+        container.className = "others-message";
+    }
+
+    const message = document.createElement("div");
+    message.className = "message";
+
+    const usr = document.createElement("p");
+    usr.className = "username";
+    usr.innerHTML = username;
+    message.appendChild(usr);
+
+    for (let paragraph of messageData.message.split("\n")) {
+        const pa = document.createElement("p");
+        pa.innerHTML = paragraph;
+        message.appendChild(pa);
+    }
+
+    const time = document.createElement("footer");
+    time.className = "time text-right";
+    time.innerHTML = messageData.sentAt;
+
+    message.appendChild(time);
+    container.appendChild(message);
+
+    return container;
+}
